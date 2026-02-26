2f3ad7ecb1203d0a870e14e7ae6604fe6d2caa06
vikrantkamatpartner <vikrant.kamat@bti.bmwgroup.com>
2026-02-26 09:37:03 +0530

    Changed .env variable to point to prod DB

--- server/config/db.js
+++ server/config/db.js
@@ -21,7 +21,7 @@ const connectDB = async () => {
     };
 
     console.log('Connecting to MongoDB Atlas...');
-    const conn = await mongoose.connect(process.env.MONGODB_URI, options);
+    const conn = await mongoose.connect(process.env.MONGODB_PROD_URI, options);
     
     console.log(`✅ MongoDB Atlas Connected: ${conn.connection.host}`);
     console.log(`📊 Database: ${conn.connection.name}`);
